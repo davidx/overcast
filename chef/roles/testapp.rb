8569ab5acaccb4de9b846e26c1f9e4710610a75f
David Andersen <davidx@gmail.com>
2010-12-28 16:04:47 -0800

    add

--- chef/roles/testapp.rb
+++ chef/roles/testapp.rb
@@ -2,5 +2,10 @@ name "testapp"
 description "testapp role"
 run_list(["recipe[ruby_custom]"])
 
+
+default_attributes {
+
+  
+}
 # Attributes applied no matter what the node has set already.
 #override_attributes()
